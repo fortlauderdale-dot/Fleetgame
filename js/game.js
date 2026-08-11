cd44ac2b81de6383bf3f6933bc90710f1154e0c3
fortlauderdale-dot <drewnewstrom@gmail.com>
2026-08-11 08:23:22 -0400

    Update game.js
    close gap between gate and fence

--- js/game.js
+++ js/game.js
@@ -288,8 +288,8 @@ rebuildCanopy();
 
 const trailer = buildAdminTrailer(); trailer.position.set(56, 0, -40); scene.add(trailer);
 const gate = buildGate(15); gate.position.set(75, 0, 20); gate.rotation.y = Math.PI / 2; scene.add(gate);
-const fenceE1 = buildFence(19); fenceE1.rotation.y = Math.PI / 2; fenceE1.position.set(75, 0, 39.5); scene.add(fenceE1);
-const fenceE2 = buildFence(59); fenceE2.rotation.y = Math.PI / 2; fenceE2.position.set(75, 0, -19.5); scene.add(fenceE2);
+const fenceE1 = buildFence(21.5); fenceE1.rotation.y = Math.PI / 2; fenceE1.position.set(75, 0, 38.25); scene.add(fenceE1);
+const fenceE2 = buildFence(61.5); fenceE2.rotation.y = Math.PI / 2; fenceE2.position.set(75, 0, -18.25); scene.add(fenceE2);
 const dumpster = buildDumpster(); dumpster.position.set(-64, 0, 40); dumpster.rotation.y = 0.5; scene.add(dumpster);
 
 const poles = [];
